3f63e0e88b2a1684aea3acea68056a9daebc01f7
ashutosh.raj2 <ashutoshraj@inito.com>
2026-08-02 16:40:36 +0530

    Initial Commit

---
+++ goCat/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  goCat
+//
+//  Created by User on 02/08/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
